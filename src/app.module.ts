a6934f7a74aac60a2e86a920f0e7c434d20fcf4a
Sean Gravener <sean@gravener.net>
2022-09-01 15:32:18 -0400

    Switch to sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,37 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ContentsModule } from './contents/contents.module';
-import { Chapter, Section } from './contents/entities/chapter.entity';
+import { Chapter } from './contents/entities/chapter.entity';
+import { Section } from './contents/entities/section.entity';
+
+// const postgres = {
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'postgres',
+//   password: 'postgres',
+//   database: 'postgres',
+//   autoLoadEntities: true,
+//   entities: [Chapter, Section],
+
+//   // Development only
+//   synchronize: true,
+// };
+
+const sqlite = {
+  type: 'sqlite',
+  database: 'db/sqlite.db',
+  autoLoadEntities: true,
+  entities: [Chapter, Section],
+
+  // Development only
+  synchronize: true,
+} as TypeOrmModuleOptions;
 
 @Module({
-  imports: [
-    ContentsModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
-      autoLoadEntities: true,
-      entities: [Chapter, Section],
-
-      // Development only
-      synchronize: true,
-    }),
-  ],
+  imports: [ContentsModule, TypeOrmModule.forRoot(sqlite)],
   controllers: [AppController],
   providers: [AppService],
 })
